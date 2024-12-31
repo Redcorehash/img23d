4cb2fe8af2abb397563bd2688ccc2877fbdbd27e
Redcorehash <ry76rt3ye@yandex.com>
2024-12-31 14:01:16 +0700

    Create cameraControls.js

---
+++ cameraControls.js
@@ -0,0 +1,5 @@
+// Add camera controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
