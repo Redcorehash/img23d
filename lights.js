388e4e6361a0a652dde9b7da8fd5e926f814034c
Redcorehash <ry76rt3ye@yandex.com>
2024-12-31 13:59:56 +0700

    Create lights.js

---
+++ lights.js
@@ -0,0 +1,7 @@
+// Add lights to the scene
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+pointLight.position.set(10, 10, 10);
+scene.add(pointLight);
