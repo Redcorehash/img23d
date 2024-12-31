701ea314f551308a70bc62590a1563e77234e31e
Redcorehash <ry76rt3ye@yandex.com>
2024-12-31 14:02:50 +0700

    Create interaction.js

---
+++ interaction.js
@@ -0,0 +1,15 @@
+// Handle object interaction
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+window.addEventListener('click', (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    if (intersects.length > 0) {
+        console.log('Object clicked:', intersects[0].object);
+    }
+});
